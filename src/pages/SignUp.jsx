2d95cf9d9aecc1c4e30fa94d326eca61b94dbbe3
dpthegrey <49444980+dpthegrey@users.noreply.github.com>
2022-03-09 18:16:33 +0530

    Save User To Firestore

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -5,7 +5,7 @@ import {
   createUserWithEmailAndPassword,
   updateProfile,
 } from "firebase/auth";
-
+import { setDoc, doc, serverTimestamp } from "firebase/firestore";
 // firebase.config.js has credentials for firebase
 import { db } from "../firebase.config";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
@@ -43,7 +43,15 @@ const SignUp = () => {
 
       const user = userCredential.user;
 
-      updateProfile(auth.currentUser, { displayName: name });
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      });
+
+      const formDataCopy = { ...formData };
+      delete formDataCopy.password;
+      formDataCopy.timestamp = serverTimestamp();
+
+      await setDoc(doc(db, "users", user.uid), formDataCopy);
 
       navigate("/");
     } catch (error) {
